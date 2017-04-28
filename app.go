c3f0f3724dd8b3c3f7fcd5552063958c76f7d313
J. Salvador Arias <jsalarias@gmail.com>
2017-04-28 17:30:19 -0300

    Command is now an interface

--- app.go
+++ app.go
@@ -24,17 +24,33 @@ package cmdapp
 import (
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"strings"
+	"sync"
 )
 
 // Short is a short description of the application.
 var Short string
 
-// Commands is the list of available commands and help topics. The order in
-// the list is used for help output.
-var Commands []*Command
+// commands is the list of available commands and help topics.
+var (
+	mutex    sync.Mutex
+	commands = make(map[string]Command)
+)
+
+// Add adds a new command to the application.
+// Command names should be unique,
+// otherwise it will trigger a panic.
+func Add(c Command) {
+	name := strings.ToLower(c.Name())
+	mutex.Lock()
+	defer mutex.Unlock()
+	if _, dup := commands[name]; dup {
+		msg := fmt.Sprintf("cmdapp: Repeated command name: %s %s", name, c.Short())
+		panic(msg)
+	}
+	commands[name] = c
+}
 
 // Name stores the application name, the default is based on the arguments of
 // the program.
@@ -49,26 +65,24 @@ func Run() {
 	if len(args) < 1 {
 		usage()
 	}
-	if args[0] == "help" {
-		help(args[1:])
-		return
+
+	mutex.Lock()
+	c, ok := commands[args[0]]
+	mutex.Unlock()
+	if !ok || !c.Runnable() {
+		fmt.Fprintf(os.Stderr, "%s: unknown subcommand %s\nRun '%s help' for usage.\n", Name, args[0], Name)
+		os.Exit(1)
 	}
 
-	for _, c := range Commands {
-		if (c.Name() == args[0]) && (c.Run != nil) {
-			c.Flag.Usage = func() { c.Usage() }
-			c.Flag.Parse(args[1:])
-			err := c.Run(c, c.Flag.Args())
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "%s: %s: %v\n", Name, c.Name(), err)
-				os.Exit(1)
-			}
-			return
-		}
+	fs := flag.NewFlagSet(c.Name(), flag.ExitOnError)
+	fs.Usage = func() { Usage(c) }
+	c.Register(fs)
+	fs.Parse(args[1:])
+	err := c.Run(fs.Args())
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%s: %s: %v\n", Name, c.Name(), err)
+		os.Exit(1)
 	}
-
-	fmt.Fprintf(os.Stderr, "%s: unknown subcommand %s\nRun '%s help' for usage.\n", Name, args[0], Name)
-	os.Exit(1)
 }
 
 // usage printd application's help and exists.
@@ -76,78 +90,3 @@ func usage() {
 	printUsage(os.Stderr)
 	os.Exit(1)
 }
-
-// printUsage outputs the application usage help.
-func printUsage(w io.Writer) {
-	fmt.Fprintf(w, "%s\n\n", Short)
-	fmt.Fprintf(w, "Usage:\n\n    %s [help] <command> [<args>...]\n\n", Name)
-	topics := false
-	fmt.Fprintf(w, "The commands are:\n")
-	for _, c := range Commands {
-		if c.Run == nil {
-			topics = true
-			continue
-		}
-		fmt.Fprintf(w, "    %-16s %s\n", c.Name(), c.Short)
-	}
-	fmt.Fprintf(w, "\nUse '%s help <command>' for more information about a command.\n\n", Name)
-	if !topics {
-		return
-	}
-	fmt.Fprintf(w, "Additional help topics:\n\n")
-	for _, c := range Commands {
-		if c.Run != nil {
-			continue
-		}
-		fmt.Fprintf(w, "    %-16s %s\n", c.Name(), c.Short)
-	}
-	fmt.Fprintf(w, "\nUse '%s help <topic>' for more information about that topic.\n\n", Name)
-}
-
-// help implements the 'help' command.
-func help(args []string) {
-	if len(args) == 0 {
-		printUsage(os.Stdout)
-		return
-	}
-	if len(args) != 1 {
-		fmt.Fprintf(os.Stderr, "%s: help: too many arguments.\nUsage: '%s help [<command>]'\n", Name, Name)
-		os.Exit(1)
-	}
-
-	arg := args[0]
-
-	// 'help documentation' generates doc.go
-	if arg == "documentation" {
-		f, err := os.Create("doc.go")
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "%s: help: %v\n", Name, err)
-			os.Exit(1)
-		}
-		defer f.Close()
-		fmt.Fprintf(f, "%s\n", strings.TrimSpace(goHead))
-		printUsage(f)
-		for _, c := range Commands {
-			c.documentation(f)
-		}
-		fmt.Fprintf(f, "\n%s", strings.TrimSpace(goFoot))
-		return
-	}
-
-	for _, c := range Commands {
-		if c.Name() == arg {
-			c.help()
-			return
-		}
-	}
-
-	fmt.Fprintf(os.Stderr, "%s: help: unknown help topic %s.\nRun '% help'\n", Name, arg, Name)
-	os.Exit(1)
-}
-
-var goHead = `// Authomatically generated doc.go file for use with godoc.
-
-/*`
-
-var goFoot = `*/
-package main`
---
+++ helpcmd.go
@@ -0,0 +1,132 @@
+// Copyright (c) 2015, J. Salvador Arias <jsalarias@gmail.com>
+// All rights reserved.
+// Distributed under BSD-style license that can be found in the LICENSE file.
+//
+// This work is derived from the go tool source code
+// Copyright 2011 The Go Authors.  All rights reserved.
+
+package cmdapp
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"strings"
+
+	"github.com/pkg/errors"
+)
+
+// help is the help command.
+type help struct{}
+
+func init() {
+	Add(help{})
+}
+
+const helpCmdLong = `
+Command help displays help information for a command or a help topic.
+
+With no arguments prints to the standard output the list of available commands
+and help topics.
+`
+
+func (h help) Name() string              { return "help" }
+func (h help) Args() string              { return "[<command>]" }
+func (h help) Short() string             { return "displays help information about " + Name }
+func (h help) Long() string              { return helpCmdLong }
+func (h help) Register(fs *flag.FlagSet) {}
+func (h help) Runnable() bool            { return true }
+
+func (h help) Run(args []string) error {
+	if len(args) == 0 {
+		printUsage(os.Stdout)
+		return nil
+	}
+	if len(args) != 1 {
+		return errors.New("help: too many arguments.")
+	}
+
+	arg := args[0]
+
+	// 'help documentation' generates doc.go
+	if arg == "documentation" {
+		f, err := os.Create("doc.go")
+		if err != nil {
+			return errors.Wrap(err, "help:")
+		}
+		defer f.Close()
+		fmt.Fprintf(f, "%s\n", strings.TrimSpace(goHead))
+		printUsage(f)
+		mutex.Lock()
+		defer mutex.Unlock()
+		var cmds []string
+		for _, c := range commands {
+			cmds = append(cmds, c.Name())
+		}
+		sort.Strings(cmds)
+
+		for _, c := range commands {
+			documentation(f, c)
+		}
+		fmt.Fprintf(f, "\n%s", strings.TrimSpace(goFoot))
+		return nil
+	}
+
+	mutex.Lock()
+	c, ok := commands[arg]
+	mutex.Unlock()
+	if !ok {
+		return errors.Errorf("help: unknown help topic: %s", arg)
+	}
+	documentation(os.Stdout, c)
+	return nil
+}
+
+// printUsage outputs the application usage help.
+func printUsage(w io.Writer) {
+	fmt.Fprintf(w, "%s\n\n", Short)
+	fmt.Fprintf(w, "Usage:\n\n    %s [help] <command> [<args>...]\n\n", Name)
+	topics := false
+	fmt.Fprintf(w, "The commands are:\n")
+
+	mutex.Lock()
+	defer mutex.Unlock()
+	var cmds []string
+	for _, c := range commands {
+		cmds = append(cmds, c.Name())
+		if !c.Runnable() {
+			topics = true
+		}
+	}
+	sort.Strings(cmds)
+
+	for _, nm := range cmds {
+		c := commands[nm]
+		if !c.Runnable() {
+			continue
+		}
+		fmt.Fprintf(w, "    %-16s %s\n", c.Name(), c.Short())
+	}
+	fmt.Fprintf(w, "\nUse '%s help <command>' for more information about a command.\n\n", Name)
+	if !topics {
+		return
+	}
+	fmt.Fprintf(w, "Additional help topics:\n\n")
+	for _, nm := range cmds {
+		c := commands[nm]
+		if c.Runnable() {
+			continue
+		}
+		fmt.Fprintf(w, "    %-16s %s\n", c.Name(), c.Short())
+	}
+	fmt.Fprintf(w, "\nUse '%s help <topic>' for more information about that topic.\n\n", Name)
+}
+
+var goHead = `// Authomatically generated doc.go file for use with godoc.
+
+/*`
+
+var goFoot = `*/
+package main`
